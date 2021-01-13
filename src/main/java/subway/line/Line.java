23fbb2e2686b5071da4971105ec2265fa5c64746
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-13 18:05:09 +0900

    feat(line) : 노선 생성 기능 개발
    - 노선 controller, dao 내 노선 생성 기능 개발
    - 노선 객체 구현

---
+++ src/main/java/subway/line/Line.java
@@ -0,0 +1,95 @@
+package subway.line;
+
+import java.util.Objects;
+
+public class Line {
+    private Long id;
+    private String name;
+    private String color;
+    private int extraFare;
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
+
+    public Line(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public Line(String name, String color, Long upStationId, Long downStationId, int distance) {
+        this.name = name;
+        this.color = color;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getExtraFare() {
+        return extraFare;
+    }
+
+    public void setExtraFare(int extraFare) {
+        this.extraFare = extraFare;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public void setUpStationId(Long upStationId) {
+        this.upStationId = upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public void setDownStationId(Long downStationId) {
+        this.downStationId = downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(name, line.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color, extraFare, upStationId, downStationId, distance);
+    }
+}
---
+++ src/main/java/subway/line/LineDao.java
@@ -0,0 +1,30 @@
+package subway.line;
+
+import org.springframework.util.ReflectionUtils;
+import subway.station.Station;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineDao {
+
+    private Long seq = 0L;
+    private List<Line> lines = new ArrayList<>();
+
+    public Line save(Line line) {
+        Line persistStation = createNewObject(line);
+        if(lines.contains(line)){
+            throw new IllegalArgumentException("노선 이름이 중복됩니다.");
+        }
+        lines.add(persistStation);
+        return persistStation;
+    }
+
+    private Line createNewObject(Line line) {
+        Field field = ReflectionUtils.findField(Line.class, "id");
+        field.setAccessible(true);
+        ReflectionUtils.setField(field, line, ++seq);
+        return line;
+    }
+}

8c258d8a86c970f721cc27338bb64f2346ae3787
sierra.s <sierra.s@kakaocorp.com>
2021-01-13 19:13:47 +0900

    feat(station): 전체 역을 조회하는 기능 추가

--- src/main/java/subway/station/StationController.java
+++ src/main/java/subway/station/StationController.java
@@ -5,26 +5,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class StationController {
 
+    private final StationDao stationDao;
+
+    public StationController() {
+        this.stationDao = new StationDao();
+    }
+
     @PostMapping("/stations")
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         Station station = new Station(stationRequest.getName());
-        Station newStation = new StationDao().save(station);
+        Station newStation = this.stationDao.save(station);
         StationResponse stationResponse = new StationResponse(newStation.getId(), newStation.getName());
         return ResponseEntity.created(URI.create("/stations/" + newStation.getId())).body(stationResponse);
     }
 
     @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
-        return ResponseEntity.ok().build();
+        List<Station> stations = stationDao.findAll();
+        List<StationResponse> stationResponses = new ArrayList<>();
+        for (Station station : stations) {
+            stationResponses.add(new StationResponse(station.getId(), station.getName()));
+        }
+        return ResponseEntity.ok().body(stationResponses);
     }
 
     @DeleteMapping("/stations/{id}")
     public ResponseEntity deleteStation(@PathVariable Long id) {
+        this.stationDao.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }

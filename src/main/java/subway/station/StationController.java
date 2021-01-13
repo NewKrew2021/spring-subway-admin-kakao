9b0fb627774aa1f2716f14d798c157675ba7a52e
SimJung <tlawjd99gma@gmail.com>
2021-01-13 17:56:26 +0900

    feat : StationController 기능 구현
     - showStations() 메소드를 통해 조회가 가능하도록 구현
     - deleteStation() 메소드를 통해 삭제가 가능하도록 구현

--- src/main/java/subway/station/StationController.java
+++ src/main/java/subway/station/StationController.java
@@ -1,30 +1,44 @@
 package subway.station;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StationController {
 
+    private final StationDao stationDao;
+
+    public StationController() {
+        this.stationDao = new StationDao();
+    }
+
     @PostMapping("/stations")
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         Station station = new Station(stationRequest.getName());
-        Station newStation = new StationDao().save(station);
+        Station newStation = stationDao.save(station);
         StationResponse stationResponse = new StationResponse(newStation.getId(), newStation.getName());
         return ResponseEntity.created(URI.create("/stations/" + newStation.getId())).body(stationResponse);
     }
 
     @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(stationDao.findAll()
+                .stream()
+                .map(station -> new StationResponse(station.getId(), station.getName()))
+                .collect(Collectors.toList()));
     }
 
     @DeleteMapping("/stations/{id}")
     public ResponseEntity deleteStation(@PathVariable Long id) {
+        stationDao.deleteById(id);
         return ResponseEntity.noContent().build();
     }
+
+
 }

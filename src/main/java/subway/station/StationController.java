651ef306fedda82c0d9c4c7c2e506cdc01144d27
brody <youoper@gmail.com>
2021-01-13 17:15:12 +0900

    feat(StationController): 목록 확인 및 삭제를 위한 showStations, deleteStation 메서드 구현

--- src/main/java/subway/station/StationController.java
+++ src/main/java/subway/station/StationController.java
@@ -6,25 +6,32 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StationController {
 
+    private final StationDao dao = StationDao.getInstance();
+
     @PostMapping("/stations")
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         Station station = new Station(stationRequest.getName());
-        Station newStation = new StationDao().save(station);
+        Station newStation = dao.save(station);
         StationResponse stationResponse = new StationResponse(newStation.getId(), newStation.getName());
         return ResponseEntity.created(URI.create("/stations/" + newStation.getId())).body(stationResponse);
     }
 
     @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
-        return ResponseEntity.ok().build();
+        List<StationResponse> responses = dao.findAll().stream()
+                .map(station -> new StationResponse(station.getId(), station.getName()))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(responses);
     }
 
     @DeleteMapping("/stations/{id}")
     public ResponseEntity deleteStation(@PathVariable Long id) {
+        dao.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }

44e16e26811115002ff4ca26ac7b87a233c7ffa0
Sangyeol Kim <syeol.kim@daum.net>
2021-01-13 17:11:43 +0900

    feat: 지하철역 목록 조회 기능 구현

--- src/main/java/subway/station/StationController.java
+++ src/main/java/subway/station/StationController.java
@@ -5,26 +5,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class StationController {
+    private final StationDao stationDao = new StationDao();
 
     @PostMapping("/stations")
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         Station station = new Station(stationRequest.getName());
-        Station newStation = new StationDao().save(station);
+        Station newStation = stationDao.save(station);
         StationResponse stationResponse = new StationResponse(newStation.getId(), newStation.getName());
         return ResponseEntity.created(URI.create("/stations/" + newStation.getId())).body(stationResponse);
     }
 
     @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
-        return ResponseEntity.ok().build();
+        return stationDao.findAll()
+                .stream()
+                .map(station -> new StationResponse(station.getId(), station.getName()))
+                .collect(Collectors.collectingAndThen(Collectors.toList(), ResponseEntity::ok));
     }
 
     @DeleteMapping("/stations/{id}")
-    public ResponseEntity deleteStation(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteStation(@PathVariable Long id) {
         return ResponseEntity.noContent().build();
     }
 }

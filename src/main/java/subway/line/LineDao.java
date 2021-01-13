082381091a839903f3746c40f193d0dbca2995f2
Sangyeol Kim <syeol.kim@daum.net>
2021-01-13 20:18:47 +0900

    feat: 지하철 노선 검색 로직과 수정 로직 추가

--- src/main/java/subway/line/LineDao.java
+++ src/main/java/subway/line/LineDao.java
@@ -5,6 +5,7 @@ import org.springframework.util.ReflectionUtils;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class LineDao {
     private Long seq = 0L;
@@ -31,6 +32,19 @@ public class LineDao {
 
     public void deleteById(Long id) {
         lines.removeIf(it -> it.getId().equals(id));
+    }
+
+    public Optional<Line> find(Long id) {
+        return lines.stream()
+                .filter(line -> line.getId().equals(id))
+                .findAny();
+    }
+
+    public void update(Line line) {
+        Line persistLine = find(line.getId())
+                .orElseThrow(() -> new IllegalArgumentException("일치하는 노선을 찾을 수 없습니다."));
+        int idx = lines.indexOf(persistLine);
+        lines.set(idx, line);
     }
 
     private Line createNewObject(Line line) {

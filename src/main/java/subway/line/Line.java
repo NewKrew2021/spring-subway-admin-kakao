94761cd5515403f7f2ad1eb2be48f850a4803e33
waylon <waylon.n@kakaocorp.com>
2021-01-14 16:33:11 +0900

    refactor: Line 리팩터링

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -1,15 +1,13 @@
 package subway.line;
 
-import org.springframework.util.ReflectionUtils;
-import subway.exceptions.InvalidLineArgumentException;
 import subway.exceptions.InvalidSectionException;
 import subway.station.StationDao;
 import subway.station.StationResponse;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 public class Line {
     private Long id;
@@ -31,44 +29,22 @@ public class Line {
         sections.add(new Section(lineRequest.getUpStationId(), lineRequest.getDownStationId(), lineRequest.getDistance()));
     }
 
-    // 1section.getUpStation == newSection.getUpStation -> 상행역의 뒤에 끼워넣음
-    // 2setcion.getDownStation == newSection.getDownStation -> 하행역의 앞에 끼워넣음
-    // 3section.getUpStation == newSection.getDownStation -> 상행역의 앞에 끼워넣음
-    // 4section.getDownStation == newSection.getUpStation -> 히헹역의 뒤에 끼워넣음
-    // 5NOTHING
-    public AddStatus findStatus(Section section, Section newSection) {
-        if(section.getUpStationId() == newSection.getDownStationId()) {
-            return AddStatus.ADD_INFRONT_UPSTATION;
-        }
-        if(section.getDownStationId() == newSection.getUpStationId()) {
-            return AddStatus.ADD_BEHIND_DOWNSTATION;
-        }
-        if (section.getUpStationId() == newSection.getUpStationId()) {
-            return AddStatus.ADD_BEHIND_UPSTATION;
-        }
-        if (section.getDownStationId() == newSection.getDownStationId()) {
-            return AddStatus.ADD_INFRONT_DOWNSTATION;
-        }
-        return AddStatus.FAIL;
-    }
-
     public boolean containsBothStationsOrNothing(Section newSection) {
         long count = sections.stream()
-                .filter(section -> (findStatus(section, newSection) != AddStatus.FAIL))
+                .filter(section -> (AddStatus.findStatus(section, newSection) != AddStatus.FAIL))
                 .count();
         return ( count % 2 ) == 0 ;
     }
 
     public List<StationResponse> getStationResponses() {
         List<StationResponse> stationResponses = new ArrayList<>();
-        for (int i = 0; i < sections.size() ; i++) {
+        for (int i = 0; i < sections.size(); i++) {
             Long stationId = sections.get(i).getUpStationId();
             stationResponses.add(new StationResponse(StationDao.findById(stationId).get()));
-            if(i == sections.size() - 1) {
-                Long downStationId = sections.get(i).getDownStationId();
-                stationResponses.add(new StationResponse(StationDao.findById(downStationId).get()));
-            }
         }
+        stationResponses.add(
+                new StationResponse(StationDao.findById(sections.get(sections.size()-1).getDownStationId()).get())
+        );
         return stationResponses;
     }
 
@@ -76,45 +52,37 @@ public class Line {
         if (containsBothStationsOrNothing(newSection)) {
             throw new InvalidSectionException("두 역이 모두 포함되어 있거나, 두 역 모두 포함되어 있지 않습니다.");
         }
+        int selectedIndex = IntStream.range(0, sections.size())
+                .filter(i -> AddStatus.findStatus(sections.get(i), newSection) != AddStatus.FAIL)
+                .findFirst()
+                .getAsInt();
 
-        for (int i = 0; i < sections.size(); i++) {
-            AddStatus addStatus = findStatus(sections.get(i), newSection);
-            if(addStatus == addStatus.ADD_BEHIND_DOWNSTATION) {
-                if(i != sections.size()-1) {
-                    Field field = ReflectionUtils.findField(Section.class, "upStationId");
-                    field.setAccessible(true);
-                    ReflectionUtils.setField(field, sections.get(i+1), newSection.getDownStationId());
-                }
-                sections.add(i+1, newSection);
-                break;
-            }
-            if (addStatus == AddStatus.ADD_INFRONT_UPSTATION) {
-                if(i != 0) {
-                    Field field = ReflectionUtils.findField(Section.class, "upStationId");
-                    field.setAccessible(true);
-                    ReflectionUtils.setField(field, sections.get(i-1), newSection.getUpStationId());
-                }
-                sections.add(i, newSection);
-                break;
-            }
-            if (addStatus == AddStatus.ADD_BEHIND_UPSTATION) {
-                int distance = sections.get(i).getDistance();
-                if(distance <= newSection.getDistance()) {
-                    throw new InvalidLineArgumentException("추가될 구간의 거리가 기존 노선 거리보다 깁니다.");
-                }
-                sections.set(i, new Section(newSection.getDownStationId(), sections.get(i).getDownStationId(), distance - newSection.getDistance()));
-                sections.add(i, new Section(newSection.getUpStationId(), newSection.getDownStationId(), newSection.getDistance()));
-                break;
-            }
-            if (addStatus == AddStatus.ADD_INFRONT_DOWNSTATION) {
-                int distance = sections.get(i).getDistance();
-                if (distance <= newSection.getDistance()) {
-                    throw new InvalidLineArgumentException("추가될 구간의 거리가 기존 노선 거리보다 깁니다.");
-                }
-                sections.set(i, new Section(sections.get(i).getUpStationId(), newSection.getUpStationId(), distance - newSection.getDistance()));
-                sections.add(i, new Section(newSection.getUpStationId(), newSection.getDownStationId(), newSection.getDistance()));
-                break;
-            }
+        saveSectionWithStatus(selectedIndex, newSection);
+    }
+
+    private void saveSectionWithStatus(int selectedIndex, Section newSection) {
+        int distance = sections.get(selectedIndex).getDistance();
+        AddStatus addStatus = AddStatus.findStatus(sections.get(selectedIndex), newSection);
+        if (addStatus == AddStatus.ADD_INFRONT_UPSTATION) {
+            sections.add(0, newSection);
+        }
+        if (addStatus == AddStatus.ADD_BEHIND_DOWNSTATION) {
+            sections.add(sections.size(), newSection);
+        }
+        validateSectionDistance(newSection, distance);
+        if (addStatus == AddStatus.ADD_BEHIND_UPSTATION) {
+            sections.set(selectedIndex, new Section(newSection.getDownStationId(), sections.get(selectedIndex).getDownStationId(), distance - newSection.getDistance()));
+            sections.add(selectedIndex, new Section(newSection.getUpStationId(), newSection.getDownStationId(), newSection.getDistance()));
+        }
+        if (addStatus == AddStatus.ADD_INFRONT_DOWNSTATION) {
+            sections.set(selectedIndex, new Section(sections.get(selectedIndex).getUpStationId(), newSection.getUpStationId(), distance - newSection.getDistance()));
+            sections.add(selectedIndex, new Section(newSection.getUpStationId(), newSection.getDownStationId(), newSection.getDistance()));
+        }
+    }
+
+    private void validateSectionDistance(Section newSection, int distance) {
+        if (distance <= newSection.getDistance()) {
+            throw new InvalidSectionException("추가될 구간의 거리가 기존 노선 거리보다 깁니다.");
         }
     }
 

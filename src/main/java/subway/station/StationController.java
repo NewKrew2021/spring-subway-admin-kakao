d270e507cbd080e3ef32094a7b372bed0025f64f
sumniy <sumniy94@gmail.com>
2021-01-14 11:53:23 +0900

    Feat: StationController 구현
    - StationDao
    - createStation
    - showStations
      - stationDao의 findAll 메서드를 통해 조회 후 반환하도록 구현
    - deleteStation
      - stationDao의 deleteById를 사용하도록 변경

--- src/main/java/subway/station/StationController.java
+++ src/main/java/subway/station/StationController.java
@@ -6,25 +6,35 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StationController {
 
+    private final StationDao stationDao = new StationDao();
+
     @PostMapping("/stations")
     public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
         Station station = new Station(stationRequest.getName());
-        Station newStation = new StationDao().save(station);
+        Station newStation = stationDao.save(station);
         StationResponse stationResponse = new StationResponse(newStation.getId(), newStation.getName());
-        return ResponseEntity.created(URI.create("/stations/" + newStation.getId())).body(stationResponse);
+        return ResponseEntity.created(URI.create("/stations/" + newStation.getId()))
+                .body(stationResponse);
     }
 
     @GetMapping(value = "/stations", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StationResponse>> showStations() {
-        return ResponseEntity.ok().build();
+        List<Station> stations = stationDao.findAll();
+        List<StationResponse> stationResponses = stations.stream()
+                .map(station -> new StationResponse(station.getId(), station.getName()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(stationResponses);
     }
 
     @DeleteMapping("/stations/{id}")
     public ResponseEntity deleteStation(@PathVariable Long id) {
+        stationDao.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }

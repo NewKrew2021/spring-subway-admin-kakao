d21828983e5d01eaf99b9cc52e55905c2a465bc4
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-15 14:19:06 +0900

    feat(section) : 구간 객체 및 DAO 정의

---
+++ src/main/java/subway/line/Section.java
@@ -0,0 +1,36 @@
+package subway.line;
+
+public class Section {
+
+    private Long id;
+    private Long lineId;
+    private Long upStationId;
+    private Long downStationId;
+    private int distance;
+
+    public Section(Long upStationId, Long downStationId, int distance) {
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Long getUpStationId() {
+        return upStationId;
+    }
+
+    public Long getDownStationId() {
+        return downStationId;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getLineId() {
+        return lineId;
+    }
+}
---
+++ src/main/java/subway/line/SectionDao.java
@@ -0,0 +1,58 @@
+package subway.line;
+
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class SectionDao {
+    private static SectionDao sectionDao = null;
+    private Long seq = 0L;
+    private List<Section> sections = new ArrayList<>();
+
+    private SectionDao() {}
+
+    public static SectionDao getInstance() {
+        if (sectionDao == null) {
+            sectionDao = new SectionDao();
+        }
+
+        return sectionDao;
+    }
+
+    public Section save(Section section) {
+        Section persistStation = createNewObject(section);
+        sections.add(persistStation);
+        return persistStation;
+    }
+
+    public List<Section> findAll() {
+        return sections;
+    }
+
+    private Section createNewObject(Section section) {
+        Field field = ReflectionUtils.findField(Line.class, "id");
+        field.setAccessible(true);
+        ReflectionUtils.setField(field, section, ++seq);
+        return section;
+    }
+
+    public void deleteById(Long id) {
+        sections.removeIf(it -> it.getId().equals(id));
+    }
+
+    public Optional<Section> findById(Long id) {
+        return sections.stream()
+                .filter(it -> it.getId() == id)
+                .findFirst();
+    }
+
+    public List<Section> findByLineId(Long lineId){
+        return sections.stream()
+                .filter(section -> section.getLineId() == lineId)
+                .collect(Collectors.toList());
+    }
+}

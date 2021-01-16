79808091f73e714160c39136f00e3382a43ea1e8
limdef <lim.s.jin22@gmail.com>
2021-01-16 21:53:40 +0900

    refactor: Section, SectionDao 3단계 적용

--- src/main/java/subway/line/Section.java
+++ src/main/java/subway/line/Section.java
@@ -3,6 +3,8 @@ package subway.line;
 import java.util.Objects;
 
 public class Section {
+    private Long id;
+    private Long lineId;
     private Long upStationId;
     private Long downStationId;
     private int distance;
@@ -10,7 +12,16 @@ public class Section {
     public Section() {
     }
 
-    public Section(Long upStationId, Long downStationId, int distance) {
+    public Section(Long lineId, Long upStationId, Long downStationId, int distance) {
+        this.lineId = lineId;
+        this.upStationId = upStationId;
+        this.downStationId = downStationId;
+        this.distance = distance;
+    }
+
+    public Section(Long id, Long lineId, Long upStationId, Long downStationId, int distance) {
+        this.id = id;
+        this.lineId = lineId;
         this.upStationId = upStationId;
         this.downStationId = downStationId;
         this.distance = distance;
@@ -20,6 +31,10 @@ public class Section {
         this.upStationId = sectionRequest.getUpStationId();
         this.downStationId = sectionRequest.getDownStationId();
         this.distance = sectionRequest.getDistance();
+    }
+
+    public Long getLineId() {
+        return lineId;
     }
 
     public Long getUpStationId() {
@@ -39,12 +54,11 @@ public class Section {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Section section = (Section) o;
-        return (Objects.equals(upStationId, section.upStationId) && Objects.equals(downStationId, section.downStationId)) ||
-                (Objects.equals(upStationId, section.downStationId) && Objects.equals(downStationId, section.upStationId));
+        return distance == section.distance && Objects.equals(lineId, section.lineId) && Objects.equals(upStationId, section.upStationId) && Objects.equals(downStationId, section.downStationId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(upStationId, downStationId, distance);
+        return Objects.hash(lineId, upStationId, downStationId, distance);
     }
 }
---
+++ src/main/java/subway/line/SectionDao.java
@@ -0,0 +1,56 @@
+package subway.line;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+@Repository
+public class SectionDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public SectionDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Section save(Section section){
+        String sql = "insert into section (line_id, up_station_id, down_station_id, distance) values (?,?,?,?)";
+
+        KeyHolder keyHoler = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(e -> {
+            PreparedStatement preparedStatement = e.prepareStatement(
+                    sql, java.sql.Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setLong(1, section.getLineId());
+            preparedStatement.setLong(2, section.getUpStationId());
+            preparedStatement.setLong(3, section.getDownStationId());
+            preparedStatement.setLong(4, section.getDistance());
+            return preparedStatement;
+        }, keyHoler);
+
+        Long id = (long) keyHoler.getKey();
+        return new Section(id, section.getLineId(), section.getUpStationId(), section.getDownStationId(), section.getDistance());
+    }
+
+    public void deleteById(Long id) {
+        jdbcTemplate.update("delete from section where id = ?", id);
+    }
+
+    public List<Section> findSectionsByLineId(Long lineId){
+        String sql = "select * from section where line_id = ?";
+        return jdbcTemplate.query(
+                sql,
+                (resultSet,rowNum)->
+                    new Section(
+                        resultSet.getLong("id"),
+                        resultSet.getLong("line_id"),
+                        resultSet.getLong("up_station_id"),
+                        resultSet.getLong("down_station_id"),
+                        resultSet.getInt("distance")
+                    )
+                ,lineId);
+    }
+}

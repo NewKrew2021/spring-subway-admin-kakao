ced45255f79d6d5580d051cca0903ca256cc713c
ddaaac <skyblue300a@gmail.com>
2021-01-16 23:06:39 +0900

    feat: Sections 일급 컬렉션 구현

--- src/main/java/subway/line/LineController.java
+++ src/main/java/subway/line/LineController.java
@@ -1,25 +1,20 @@
 package subway.line;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import subway.exception.LineNotFoundException;
 import subway.exception.StationNotFoundException;
-import subway.section.Section;
-import subway.section.SectionDao;
-import subway.section.SectionFactory;
-import subway.section.SectionRequest;
+import subway.section.*;
 import subway.station.Station;
 import subway.station.StationDao;
 import subway.station.StationResponse;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @RestController
@@ -60,12 +55,21 @@ public class LineController {
     @GetMapping(value = "/lines/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<LineResponse> showLine(@PathVariable Long id) {
         Line line = lineDao.findById(id).orElseThrow(() -> new LineNotFoundException(id));
-        List<Station> stations = getStationsByLine(line);
+        Sections sections = new Sections(sectionDao.findByLineId(line.getId()));
 
-        List<StationResponse> stationResponses = stations.stream()
+        List<StationResponse> stationResponses = getSortedStations(sections)
+                .stream()
                 .map(station -> new StationResponse(station.getId(), station.getName()))
                 .collect(Collectors.toList());
         return ResponseEntity.ok(new LineResponse(line.getId(), line.getName(), line.getColor(), stationResponses));
+    }
+
+    private List<Station> getSortedStations(Sections sections) {
+        return sections.getSortedStationIds()
+                .stream()
+                .map(stationId -> stationDao.findById(stationId)
+                        .orElseThrow(() -> new StationNotFoundException(stationId)))
+                .collect(Collectors.toList());
     }
 
     @GetMapping(value = "/lines", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -95,7 +99,7 @@ public class LineController {
 
     @PostMapping("/lines/{lineId}/sections")
     public ResponseEntity<Void> createSection(@PathVariable Long lineId, @RequestBody SectionRequest sectionRequest) {
-        List<Section> sections = sectionDao.findByLineId(lineId);
+        Sections sections = new Sections(sectionDao.findByLineId(lineId));
 
         Section newSection = new Section(
                 lineId,
@@ -104,71 +108,30 @@ public class LineController {
                 sectionRequest.getDistance()
         );
 
-        Section insertableSection = findInsertableSection(sections, newSection);
-        Section residualSection = insertableSection.subtractWith(newSection);
-
-        sectionDao.delete(insertableSection);
+        Section insertTargetSection = sections.findBySameUpOrDownStationWith(newSection);
+        Section residualSection = insertTargetSection.subtractWith(newSection);
+        sectionDao.delete(insertTargetSection);
         sectionDao.save(newSection);
         sectionDao.save(residualSection);
+
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/lines/{lineId}/sections")
     public ResponseEntity<Void> deleteSections(@PathVariable Long lineId, @RequestParam Long stationId) {
-        List<Section> sections = sectionDao.findByLineId(lineId);
-        if (sections.size() == 3) {
+        Sections sections = new Sections(sectionDao.findByLineId(lineId));
+        if (sections.isInitialState()) {
             throw new IllegalStateException("해당 노선은 지하철역을 삭제할 수 없습니다.");
         }
 
-        List<Section> connectedSections = findSectionsByStationId(sections, stationId);
-        if (connectedSections.isEmpty()) {
-            throw new IllegalArgumentException("노선에 포함되지 않은 지하철역 입니다.");
-        }
-
-        Section first = connectedSections.get(0);
-        Section second = connectedSections.get(1);
+        Pair<Section, Section> connectedSections = sections.findByStationId(stationId);
+        Section first = connectedSections.getLeft();
+        Section second = connectedSections.getRight();
         Section joinedSection = first.mergeWith(second);
 
         sectionDao.delete(first);
         sectionDao.delete(second);
         sectionDao.save(joinedSection);
         return ResponseEntity.ok().build();
-    }
-
-    private List<Station> getStationsByLine(Line line) {
-        List<Section> sections = sectionDao.findByLineId(line.getId());
-        Map<Long, Section> sectionCache = sections.stream()
-                .collect(Collectors.toMap(Section::getUpStationId, Function.identity()));
-
-        Section curr = sections.stream()
-                .filter(Section::isUpTerminal)
-                .findFirst()
-                .orElseThrow(AssertionError::new);
-
-        List<Station> stations = new ArrayList<>();
-        while (!curr.isDownTerminal()) {
-            Long id = curr.getId();
-            stations.add(
-                    stationDao.findById(curr.getDownStationId())
-                            .orElseThrow(() -> new StationNotFoundException(id))
-            );
-
-            curr = sectionCache.get(curr.getDownStationId());
-        }
-        return stations;
-    }
-
-    private Section findInsertableSection(List<Section> sections, Section newSection) {
-        return sections.stream()
-                .filter(section -> section.hasSameUpStation(newSection)
-                        || section.hasSameDownStation(newSection))
-                .findAny()
-                .orElseThrow(() -> new IllegalArgumentException("삽입할 수 있는 구간이 없습니다."));
-    }
-
-    private List<Section> findSectionsByStationId(List<Section> sections, Long stationId) {
-        return sections.stream()
-                .filter(section -> section.containsStation(stationId))
-                .collect(Collectors.toList());
     }
 }

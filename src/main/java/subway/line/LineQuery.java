625c4bba335bf1264b32d9616e2b41f39f29bb9d
jon.snow <jon.snow@kakaocorp.com>
2021-01-19 08:40:52 +0900

    refactor(query): select 쿼리에서 컬럼명들을 명시하도록 변경

--- src/main/java/subway/line/LineQuery.java
+++ src/main/java/subway/line/LineQuery.java
@@ -3,8 +3,8 @@ package subway.line;
 class LineQuery {
     public static final String insertQuery = "insert into line (name, color) values(?, ?)";
     public static final String updateQuery = "update line set name = ?, color = ? where id = ?";
-    public static final String selectByIdQuery = "select * from line where id = ?";
-    public static final String selectAllQuery = "select * from line";
+    public static final String selectByIdQuery = "select id, name, color from line where id = ?";
+    public static final String selectAllQuery = "select id, name, color from line";
     public static final String deleteByIdQuery = "delete from line where id = ?";
     public static final String countByNameQuery = "select count(*) from line where name = ?";
 }
--- src/main/java/subway/section/SectionQuery.java
+++ src/main/java/subway/section/SectionQuery.java
@@ -2,6 +2,6 @@ package subway.section;
 
 public class SectionQuery {
     public static final String insertQuery = "insert into section (line_id, up_station_id, down_station_id, distance) values(?, ?, ?, ?)";
-    public static final String selectByIdQuery = "select * from section where line_id = ?";
+    public static final String selectByIdQuery = "select id, line_id, up_station_id, down_station_id, distance from section where line_id = ?";
     public static final String deleteByIdQuery = "delete from section where id = ?";
 }

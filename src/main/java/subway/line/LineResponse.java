7e86a7ecbe23dc8477dd6748e47579297e6f7d26
Jjungs7 <jeongy95@gmail.com>
2021-01-19 01:12:18 +0900

    refactor(Jackson): 빈 생성자 삭제
    - 빈 생성자 대신 Jackson에서 사용할 파라미터를 넘기기

--- src/main/java/subway/line/LineResponse.java
+++ src/main/java/subway/line/LineResponse.java
@@ -2,6 +2,7 @@ package subway.line;
 
 import subway.station.StationResponse;
 
+import java.beans.ConstructorProperties;
 import java.util.List;
 
 public class LineResponse {
@@ -10,8 +11,7 @@ public class LineResponse {
     private String color;
     private List<StationResponse> stations;
 
-    public LineResponse() { }
-
+    @ConstructorProperties({"id", "name", "color", "stations"})
     public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
         this.id = id;
         this.name = name;

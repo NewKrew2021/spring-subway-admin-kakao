df3eb58f71700a095bd23ec2a6353624ba048fa1
Jjungs7 <jeongy95@gmail.com>
2021-01-19 02:54:00 +0900

    refactor(line): 조건 확인 방법 수정

--- src/main/java/subway/line/Line.java
+++ src/main/java/subway/line/Line.java
@@ -1,23 +1,39 @@
 package subway.line;
 
-import subway.station.StationResponse;
+import org.apache.commons.lang3.StringUtils;
+import subway.station.Stations;
 
-import java.util.List;
 import java.util.Objects;
 
 public class Line {
-    private Long id;
-    private String name;
-    private String color;
+    private final Long id;
+    private final String name;
+    private final String color;
+
+    public Line(String name, String color) {
+        this(0L, name, color);
+    }
 
     public Line(Long id, String name, String color) {
+        if (isNegative(id)) {
+            throw new IllegalArgumentException("Line ID cannot be negative");
+        }
+
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException("Line name should not be null or blank");
+        }
+
+        if (StringUtils.isBlank(color)) {
+            throw new IllegalArgumentException("Line color should not be null or blank");
+        }
+
         this.id = id;
         this.name = name;
         this.color = color;
     }
 
-    public LineResponse toDto(List<StationResponse> stations) {
-        return new LineResponse(id, name, color, stations);
+    public LineResponse toDto(Stations stations) {
+        return new LineResponse(id, name, color, stations.allToDto());
     }
 
     public Long getId() {
@@ -34,8 +50,14 @@ public class Line {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
         Line line = (Line) o;
         return Objects.equals(name, line.name);
     }
@@ -43,5 +65,9 @@ public class Line {
     @Override
     public int hashCode() {
         return Objects.hash(id, name, color);
+    }
+
+    private boolean isNegative(Long id) {
+        return id < 0;
     }
 }

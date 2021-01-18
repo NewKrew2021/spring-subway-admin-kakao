84efd35f9c4bebeac3b05820457134393b758bce
jon.snow <jon.snow@kakaocorp.com>
2021-01-19 08:40:52 +0900

    refactor(query): select 쿼리에서 컬럼명들을 명시하도록 변경

--- src/main/java/subway/station/StationQuery.java
+++ src/main/java/subway/station/StationQuery.java
@@ -2,8 +2,8 @@ package subway.station;
 
 class StationQuery {
     public static final String insertQuery = "insert into station (name) values(?)";
-    public static final String selectAllQuery = "select * from station";
-    public static final String selectByIdQuery = "select * from station where id = ?";
+    public static final String selectAllQuery = "select id, name from station";
+    public static final String selectByIdQuery = "select id, name from station where id = ?";
     public static final String deleteByIdQuery = "delete from station where id = ?";
     public static final String countByNameQuery = "select count(*) from station where name = ?";
 }

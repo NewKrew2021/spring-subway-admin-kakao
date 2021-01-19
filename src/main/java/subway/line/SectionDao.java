9ec19dc996a3c1e66614b13e453bd33e4e7c2f7c
Jjungs7 <jeongy95@gmail.com>
2021-01-19 16:18:21 +0900

    refactor(sections): sections에 위임, Section 스키마 변경
    - 기존 스키마는 다른 행에 종속족이어서 CRUD Operation 하는데에 어려움이 있었음

--- src/main/java/subway/line/SectionDao.java
+++ src/main/java/subway/line/SectionDao.java
@@ -1,6 +1,5 @@
 package subway.line;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -13,194 +12,58 @@ public class SectionDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public boolean insert(Long lineId, Section section) {
-        Sections sections = findByLineId(lineId);
-        if (isNewLine(sections)) {
-            insertInNewLine(lineId, section);
-            return true;
-        }
-
-        Section upSection = findByStationId(section.getLineId(), section.getUpStationId());
-        Section downSection = findByStationId(section.getLineId(), section.getDownStationId());
-        if (notInsertableSections(upSection, downSection)) {
+    public boolean insertOnCreateLine(Section upSection, Section downSection) {
+        if (upSection.equals(downSection)) {
             return false;
         }
 
-        if (isNewSection(upSection) && findByDownStationId(lineId, section.getUpStationId()) == null) {
-            Section newSection = new Section(lineId,
-                    section.getUpStationId(),
-                    section.getDownStationId(),
-                    section.getDistance());
-            insertSection(newSection);
-
-            return true;
-        }
-
-        if (isNewSection(downSection) && findByUpStationId(lineId, section.getDownStationId()) == null) {
-            Section newSection = new Section(lineId,
-                    section.getUpStationId(),
-                    section.getDownStationId(),
-                    section.getDistance());
-            insertSection(newSection);
-
-            return true;
-        }
-
-        Section firstSection, secondSection;
-        if (!isNewSection(upSection)) {
-            try {
-                firstSection = new Section(upSection.getId(), lineId,
-                        upSection.getUpStationId(),
-                        section.getDownStationId(),
-                        section.getDistance());
-
-                secondSection = new Section(lineId,
-                        section.getDownStationId(),
-                        upSection.getDownStationId(),
-                        upSection.getDistance() - section.getDistance());
-            } catch (IllegalArgumentException ignored) {
-                return false;
-            }
-        } else {
-            try {
-                firstSection = new Section(downSection.getId(), lineId,
-                        downSection.getUpStationId(),
-                        section.getUpStationId(),
-                        downSection.getDistance() - section.getDistance());
-
-                secondSection = new Section(lineId,
-                        section.getUpStationId(),
-                        downSection.getDownStationId(),
-                        section.getDistance());
-            } catch (IllegalArgumentException ignored) {
-                return false;
-            }
-        }
-
-        updateSection(firstSection);
-        insertSection(secondSection);
+        insertSection(upSection);
+        insertSection(downSection);
 
         return true;
     }
 
-    public boolean delete(Long lineId, Long stationId) {
-        Sections sections = findByLineId(lineId);
-        if (sections.hasOnlyOne()) {
+    public boolean insert(Section upSection, Section downSection) {
+        Sections sections = findByLineId(upSection.getLineId());
+
+        Section newSection = sections.insert(upSection, downSection);
+        if (newSection == null) {
             return false;
         }
 
-        Section upSection = findByDownStationId(lineId, stationId);
-        Section downSection = findByUpStationId(lineId, stationId);
-
-        if (neitherExists(upSection, downSection)) {
-            return false;
-        }
-
-        if (bothExists(upSection, downSection)) {
-            int distance = upSection.getDistance() + downSection.getDistance();
-            Section section = new Section(upSection.getId(), lineId,
-                    upSection.getUpStationId(), downSection.getDownStationId(), distance);
-
-            updateSection(section);
-            deleteSection(downSection);
-            return true;
-        }
-
-        deleteSection(isNewSection(upSection) ? downSection : upSection);
+        insertSection(newSection);
         return true;
     }
 
-    public Sections findByLineId(Long lineId) {
-        String sql = "select * from section where line_id = ?";
-        return new Sections(jdbcTemplate.query(sql, sectionRowMapper, lineId));
-    }
-
-    public Section findByUpStationId(Long lineId, Long upStationId) {
-        String sql = "select * from section where up_station_id = ? and line_id = ?";
-        try {
-            return jdbcTemplate.queryForObject(sql, sectionRowMapper, upStationId, lineId);
-        } catch (DataAccessException e) {
-            return null;
+    public boolean delete(Section section) {
+        Sections sections = findByLineId(section.getLineId());
+        if (sections.hasOnlyTwoSections()) {
+            return false;
         }
-    }
 
-    private boolean isNewLine(Sections sections) {
-        return sections.empty();
-    }
-
-    private void insertInNewLine(Long lineId, Section section) {
-        String sql = "insert into section (line_id, up_station_id, down_station_id, distance) values(?, ?, ?, ?)";
-        jdbcTemplate.update(sql, lineId, Section.TERMINAL_ID, section.getUpStationId(), 0);
-
-        jdbcTemplate.update(sql,
-                lineId,
-                section.getUpStationId(),
-                section.getDownStationId(),
-                section.getDistance());
-
-        jdbcTemplate.update(sql, lineId, section.getDownStationId(), Section.TERMINAL_ID, 0);
+        deleteSection(section);
+        return true;
     }
 
     private boolean insertSection(Section section) {
-        String sql = "insert into section (line_id, up_station_id, down_station_id, distance) values(?, ?, ?, ?)";
-        return jdbcTemplate.update(sql,
-                section.getLineId(),
-                section.getUpStationId(),
-                section.getDownStationId(),
-                section.getDistance()) > 0;
-    }
-
-    private boolean updateSection(Section section) {
-        String sql = "update section set up_station_id = ?, down_station_id = ?, distance = ? where id = ?";
-        return jdbcTemplate.update(sql,
-                section.getUpStationId(),
-                section.getDownStationId(),
-                section.getDistance(),
-                section.getId()) > 0;
+        String sql = "insert into section (line_id, station_id, distance) values(?, ?, ?)";
+        return jdbcTemplate.update(sql, section.getLineId(), section.getStationId(), section.getDistance()) > 0;
     }
 
     private boolean deleteSection(Section section) {
-        String sql = "delete from section where id = ?";
-        return jdbcTemplate.update(sql, section.getId()) > 0;
+        String sql = "delete from section where line_id = ? and station_id = ?";
+        return jdbcTemplate.update(sql, section.getLineId(), section.getStationId()) > 0;
     }
 
-    private Section findByStationId(Long lineId, Long stationId) {
-        Section upSection = findByUpStationId(lineId, stationId);
-        Section downSection = findByDownStationId(lineId, stationId);
-
-        return isNewSection(upSection) ? downSection : upSection;
-    }
-
-    private Section findByDownStationId(Long lineId, Long downStationId) {
-        String sql = "select * from section where down_station_id = ? and line_id = ?";
-        try {
-            return jdbcTemplate.queryForObject(sql, sectionRowMapper, downStationId, lineId);
-        } catch (DataAccessException e) {
-            return null;
-        }
-    }
-
-    private boolean notInsertableSections(Section upSection, Section downSection) {
-        return neitherExists(upSection, downSection) || bothExists(upSection, downSection);
-    }
-
-    private boolean bothExists(Section upSection, Section downSection) {
-        return !isNewSection(upSection) && !isNewSection(downSection);
-    }
-
-    private boolean neitherExists(Section upSection2, Section downSection2) {
-        return isNewSection(upSection2) && isNewSection(downSection2);
-    }
-
-    private boolean isNewSection(Section upSection) {
-        return upSection == null;
+    public Sections findByLineId(Long lineId) {
+        String sql = "select * from section where line_id = ? order by distance";
+        return new Sections(jdbcTemplate.query(sql, sectionRowMapper, lineId));
     }
 
     private final RowMapper<Section> sectionRowMapper =
             (resultSet, rowNum) -> new Section(
                     resultSet.getLong("id"),
                     resultSet.getLong("line_id"),
-                    resultSet.getLong("up_station_id"),
-                    resultSet.getLong("down_station_id"),
+                    resultSet.getLong("station_id"),
                     resultSet.getInt("distance"));
 }

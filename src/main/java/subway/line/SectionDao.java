be77eaa64c519792e1d2456716ce6ed1bce15b20
pharrell.mp3 <pharrell.mp3@kakaocorp.com>
2021-01-19 14:52:47 +0900

    refactor(line) : Section DB 연산에 사용되는 쿼리 enum 클래스에 정의

--- src/main/java/subway/line/SectionDao.java
+++ src/main/java/subway/line/SectionDao.java
@@ -22,36 +22,26 @@ public class SectionDao {
     SectionMapper sectionMapper;
 
     public void save(Section section) {
-        jdbcTemplate.update("insert into SECTION (line_id, up_station_id, down_station_id, distance) values (?, ?, ?, ?)",
+        jdbcTemplate.update(SectionQuery.SAVE.getQuery(),
                 section.getLineId(), section.getUpStationId(), section.getDownStationId(), section.getDistance());
     }
 
-    public List<Section> findAll() {
-        String sql = "select * from SECTION";
-        return jdbcTemplate.query(sql, sectionMapper);
-    }
-
     public void deleteById(Long id) {
-        jdbcTemplate.update("delete from SECTION where id = ?",id);
-    }
-
-    public Section findById(Long id) {
-        String sql = "select * from SECTION where id = ?";
-        return jdbcTemplate.queryForObject(sql, sectionMapper, id);
+        jdbcTemplate.update(SectionQuery.DELETE_BY_ID.getQuery(),id);
     }
 
     public List<Section> findByLineId(Long lineId){
-        String sql = "select * from SECTION where line_id = ?";
-        return jdbcTemplate.query(sql, sectionMapper, lineId);
+        return jdbcTemplate.query(SectionQuery.FIND_BY_LINE_ID.getQuery(), sectionMapper, lineId);
     }
 
     public void update(Section updateSection) {
-        jdbcTemplate.update("update SECTION set up_station_id = ?, down_station_id = ?, distance = ? where id = ?",
-                updateSection.getUpStationId(), updateSection.getDownStationId(), updateSection.getDistance(), updateSection.getId());
+        jdbcTemplate.update(SectionQuery.UPDATE.getQuery(),
+                updateSection.getUpStationId(), updateSection.getDownStationId(),
+                updateSection.getDistance(), updateSection.getId());
     }
 
     public List<Section> findByStationIdAndLineId(Long stationId, Long lineId) {
-        String sql = "select * from SECTION where line_id = ? and (up_station_id = ? or down_station_id = ?)";
-        return jdbcTemplate.query(sql, sectionMapper, lineId, stationId, stationId);
+        return jdbcTemplate.query(SectionQuery.FIND_BY_STATION_ID_AND_LINE_ID.getQuery(),
+                sectionMapper, lineId, stationId, stationId);
     }
 }
---
+++ src/main/java/subway/line/SectionQuery.java
@@ -0,0 +1,20 @@
+package subway.line;
+
+public enum SectionQuery {
+    SAVE("insert into SECTION (line_id, up_station_id, down_station_id, distance) values (?, ?, ?, ?)"),
+    DELETE_BY_ID("delete from SECTION where id = ?"),
+    FIND_BY_LINE_ID("select * from SECTION where line_id = ?"),
+    UPDATE("update SECTION set up_station_id = ?, down_station_id = ?, distance = ? where id = ?"),
+    FIND_BY_STATION_ID_AND_LINE_ID("select * from SECTION where line_id = ? and (up_station_id = ? or down_station_id = ?)")
+    ;
+
+    private SectionQuery(java.lang.String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    private String query;
+}

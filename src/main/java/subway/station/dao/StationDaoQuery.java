9b4c5b9f5ecb468fc313a5c1122178f253d32eb6
harim <harim9504@gmail.com>
2021-01-21 22:29:42 +0900

    refactor: modify StationDaoQuery

--- src/main/java/subway/station/dao/StationDaoQuery.java
+++ src/main/java/subway/station/dao/StationDaoQuery.java
@@ -1,9 +1,9 @@
 package subway.station.dao;
 
 public class StationDaoQuery {
-    public static final String INSERT = "insert into station (name) values(?)";
-    public static final String FIND_ALL = "select * from station";
-    public static final String FIND_BY_ID = "select id, name from station where id = ?";
-    public static final String DELETE = "delete from station where id = ?";
-    public static final String COUNT_BY_NAME = "select count(*) from station where name = ?";
+    public static final String INSERT = "insert into STATION (name) values(?)";
+    public static final String FIND_ALL = "select * from STATION";
+    public static final String FIND_BY_ID = "select id, name from STATION where id = ?";
+    public static final String DELETE = "delete from STATION where id = ?";
+    public static final String COUNT_BY_NAME = "select count(*) from STATION where name = ?";
 }

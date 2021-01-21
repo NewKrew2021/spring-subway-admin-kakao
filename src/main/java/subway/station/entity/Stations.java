a53c49e5772172042dfbea2b2d0546013064d8e5
Sangyeol Kim <syeol.kim@daum.net>
2021-01-22 00:49:23 +0900

    feat: 유효성 검사 기능 추가

--- src/main/java/subway/station/entity/Stations.java
+++ src/main/java/subway/station/entity/Stations.java
@@ -8,7 +8,28 @@ public class Stations {
     private final List<Station> stations;
 
     public Stations(List<Station> stations) {
+        validate(stations);
         this.stations = Collections.unmodifiableList(stations);
+    }
+
+    private void validate(List<Station> stations) {
+        if (isEmpty(stations)) {
+            throw new IllegalArgumentException("비어있는 지하철 역 리스트를 입력받을 수 없습니다.");
+        }
+        if (hasDuplicateName(stations)) {
+            throw new IllegalArgumentException("동일한 이름을 가진 지하철 역을 입력받을 수 없습니다.");
+        }
+    }
+
+    private boolean isEmpty(List<Station> stations) {
+        return stations == null || stations.isEmpty();
+    }
+
+    private boolean hasDuplicateName(List<Station> stations) {
+        return stations.stream()
+                .map(Station::getName)
+                .distinct()
+                .count() != stations.size();
     }
 
     public Stream<Station> stream() {

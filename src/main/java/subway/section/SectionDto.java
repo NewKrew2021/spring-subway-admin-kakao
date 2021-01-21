a19f5400fc53c14a85d6906e266a65e009c5549e
Eli Nabro <eli.nabro@kakaocorp.com>
2021-01-22 03:20:46 +0900

    refactor : SectionUpdateType의 데이터를 Dto로 이동
    enum class의 동시성 문제를 해결하기 위함

--- src/main/java/subway/section/SectionDto.java
+++ src/main/java/subway/section/SectionDto.java
@@ -5,11 +5,22 @@ public class SectionDto {
     private Long upStationId;
     private Long downStationId;
     private int distance;
+    private Long lineId;
+    private Long stationId;
 
-    public SectionDto(SectionRequest sectionRequest) {
+    private Section targetSection;
+    private Section prevSection;
+
+    public SectionDto(Long lineId, Long stationId) {
+        this.lineId = lineId;
+        this.stationId = stationId;
+    }
+
+    public SectionDto(SectionRequest sectionRequest, Long lineId) {
         this.upStationId = sectionRequest.getUpStationId();
         this.downStationId = sectionRequest.getDownStationId();
         this.distance = sectionRequest.getDistance();
+        this.lineId = lineId;
     }
 
     public Long getUpStationId() {
@@ -22,4 +33,35 @@ public class SectionDto {
         return distance;
     }
 
+    public void setPrevSections(Section prevSection) {
+        this.prevSection = prevSection;
+    }
+
+    public void setTargetSection(Section newSection) {
+        this.targetSection = newSection;
+    }
+
+    public Section getTargetSection() {
+        return targetSection;
+    }
+
+    public Section getPrevSection() {
+        return prevSection;
+    }
+
+    public Long getLineId() {
+        return lineId;
+    }
+
+    public void setLineId(Long lineId) {
+        this.lineId = lineId;
+    }
+
+    public Long getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(Long stationId) {
+        this.stationId = stationId;
+    }
 }
--- src/main/java/subway/section/SectionUpdateType.java
+++ src/main/java/subway/section/SectionUpdateType.java
@@ -1,59 +1,36 @@
 package subway.section;
 
+import subway.exception.TooLongDistanceSectionException;
+
 public enum SectionUpdateType {
 
     INSERT_DOWN_SECTION,
-    INSERT_UP_SECTION,
-    DELETE_SECTION;
+    INSERT_UP_SECTION;
 
-    private Section targetSection;
-    private Section prevSection;
+    public void updateDistanceAsInsert(SectionDto sectionDto) {
+        Section targetSection = sectionDto.getTargetSection();
+        Section prevSection = sectionDto.getPrevSection();
 
-
-    public boolean invalidateDistanceAsInsert() {
-        if( targetSection.getNextStationId().equals(Section.WRONG_ID) || prevSection == Section.DO_NOT_EXIST_SECTION ){
-            return false;
+        if( this.invalidateDistanceAsInsert(targetSection, prevSection) ) {
+            throw new TooLongDistanceSectionException();
         }
-        return targetSection.getDistance() >= prevSection.getDistance();
-    }
 
-    public void updateDistanceAsInsert() {
         if( this == INSERT_DOWN_SECTION) {
             int distance = targetSection.getDistance();
             targetSection.setDistance((prevSection.getDistance() == 0) ? 0 :  prevSection.getDistance() - targetSection.getDistance());
             prevSection.setDistance(distance);
         }
+
         if( this == INSERT_UP_SECTION && prevSection != Section.DO_NOT_EXIST_SECTION ) {
             prevSection.setDistance(prevSection.getDistance() - targetSection.getDistance());
         }
     }
 
-    public void updatePrevSectionAsInsert() {
-        if( this.prevSection != Section.DO_NOT_EXIST_SECTION ) {
-            this.prevSection.setNextStation(this.targetSection.getStationId());
+    public boolean invalidateDistanceAsInsert(Section targetSection, Section prevSection) {
+        if( targetSection.getNextStationId().equals(Section.WRONG_ID) || prevSection == Section.DO_NOT_EXIST_SECTION ){
+            return false;
         }
-    }
-
-    public void updatePrevSectionAsDelete() {
-        if( this.prevSection != Section.DO_NOT_EXIST_SECTION ) {
-            this.prevSection.setNextStation(this.targetSection.getNextStationId());
-        }
-    }
-
-    public void setPrevSections(Section prevSection) {
-        this.prevSection = prevSection;
-    }
-
-    public void setTargetSection(Section newSection) {
-        this.targetSection = newSection;
-    }
-
-    public Section getTargetSection() {
-        return targetSection;
-    }
-
-    public Section getPrevSection() {
-        return prevSection;
+        return targetSection.getDistance() >= prevSection.getDistance();
     }
 
 }
